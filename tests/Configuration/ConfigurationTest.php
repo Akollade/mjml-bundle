591daf7ed81d01686651d8f75c9557f6aa7512cd
Floran Brutel <contact@floran.fr>
2018-11-18 13:15:39 +0100

    Add test of configuration

---
+++ tests/Configuration/ConfigurationTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace NotFloran\MjmlBundle\Tests\Configuration;
+
+use Matthias\SymfonyConfigTest\PhpUnit\ConfigurationTestCaseTrait;
+use NotFloran\MjmlBundle\DependencyInjection\Configuration;
+use PHPUnit\Framework\TestCase;
+
+class ConfigurationTest extends TestCase
+{
+    use ConfigurationTestCaseTrait;
+
+    protected function getConfiguration()
+    {
+        return new Configuration();
+    }
+
+    public function testBinaryConfiguration()
+    {
+        $config = [
+            [
+                'renderer' => 'binary',
+                'options' => [
+                    'binary' => 'mjml',
+                    'minify' => true,
+                ],
+            ]
+        ];
+
+        $this->assertConfigurationIsValid($config);
+    }
+
+    public function testInvalidBinaryConfiguration()
+    {
+        $config = [
+            [
+                'renderer' => 'binary',
+                'options' => [
+                    'binary' => null,
+                ],
+            ]
+        ];
+
+        $this->assertConfigurationIsInvalid($config, "Binary is missing");
+    }
+
+    public function testServiceConfiguration()
+    {
+        $config = [
+            [
+                'renderer' => 'service',
+                'options' => [
+                    'service_id' => 'App\Mjml\TestRenderer',
+                ],
+            ]
+        ];
+
+        $this->assertConfigurationIsValid($config);
+    }
+
+    public function testInvalidServiceConfiguration()
+    {
+        $config = [
+            [
+                'renderer' => 'service',
+                'options' => [],
+            ]
+        ];
+
+        $this->assertConfigurationIsInvalid($config, '"service_id" is missing for service renderer');
+    }
+}
