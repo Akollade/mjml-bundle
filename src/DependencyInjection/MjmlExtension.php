600d84a037141d58016ca78b510cb635ef1b068d
Floran Brutel <notFloran@users.noreply.github.com>
2023-07-04 08:16:12 +0200

    Add return void in MjmlExtension

--- src/DependencyInjection/MjmlExtension.php
+++ src/DependencyInjection/MjmlExtension.php
@@ -12,6 +12,9 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class MjmlExtension extends Extension
 {
+    /**
+     * @return void
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
